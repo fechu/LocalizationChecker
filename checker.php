1cb2f04192a2fe2ec7b4fce7e122003493f54001
Sandro Meier <sandro.meier@fidelisfactory.ch>
2014-07-16 22:57:27 +0200

    Add basic skeleton for checker.

--- checker.php
+++ checker.php
@@ -1,4 +1,11 @@
 <?php
 require_once('vendor/autoload.php');
 
+use Symfony\Component\Console\Application;
 
+$application = new Application();
+
+// Add commands to applicatio
+
+
+$application->run();
