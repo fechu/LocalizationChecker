714da28d4c7e30b648780614507dcdd8ea58061a
Sandro Meier <sandro.meier@fidelisfactory.ch>
2014-07-17 20:59:42 +0200

    Implement Lexer funcitonality and many unit tests.

--- src/SM/Lexer/Lexer.php
+++ src/SM/Lexer/Lexer.php
@@ -27,8 +27,9 @@ class Lexer
      */
     public function __construct($tokenDefinitions)
     {
+        // If the argument is not an array, put it into an array.
         if (!is_array($tokenDefinitions)) {
-            throw new \InvalidArgumentException("tokenDefinitions need to be an array.");
+            $tokenDefinitions = array($tokenDefinitions);
         }
 
         // Check if all tokenDefinitions are correct objects
@@ -51,13 +52,89 @@ class Lexer
      *
      * @param string $text  The text that should be tokenized
      *
-     * @throws Exception    When the given text cannot be parsed. The exception message
-     *                      contains additional details about the error that occured.
+     * @throws InvalidArgumentException     When the given text cannot be parsed. The exception 
+     *                                      message contains additional details about the error 
+     *                                      that occured.
      * @return array        An array of SM\Lexer\Token objects. 
      */
     public function tokenize( $text )
     {
+        // Divide the text into lines
+        $text = str_replace("\r\n", "\n", $text);
+        $lines = explode("\n", $text);
         
+        // Tokenize all lines.
+        $tokens = array();
+        $lineNumber = 1;
+        foreach ($lines as $line){
+            $lineTokens = $this->tokenizeLine($line, $lineNumber);
+            $tokens = array_merge($tokens, $lineTokens);
+            $lineNumber++;
+        }
+        
+        return $tokens;
+    }
+
+    /**
+     * Tokenizes a single line. 
+     *
+     * @param string    $line       A line of text which may not contain any newline characters.
+     * @param integer   $lineNumber The number of the line that is currently parsed. 
+     *
+     * @return array    An array with tokens
+     *
+     * @throws InvalidArgumentException When the line cannot be parsed.
+     */
+    protected function tokenizeLine($line, $lineNumber = 1)
+    {
+        $tokenDefCount = count($this->tokenDefinitions);
+
+        // variable initialization
+        $i = 0;
+        $match = false;
+        $offset = 0;
+        $tokens = array();
+        
+        // Check token definitions as long as we don't have one.
+        while (($i < $tokenDefCount) && strlen($line) > 0) {
+
+            // Trim the line
+            $oldLineLength = strlen($line);
+            $line = ltrim($line);
+            $offset += $oldLineLength - strlen($line);
+
+            $tokenDef = $this->tokenDefinitions[$i];
+            $regex = "/^" . $tokenDef->getRegex() . "/";
+
+            $matchFound = preg_match($regex, $line, $matches);
+            if ($matchFound) {
+                // Create a new token. We had a match.
+                $token = new Token($tokenDef->getRegex(), $tokenDef->getIdentifier());
+                $token->setMatch($matches, $lineNumber, $offset);
+                
+                // Adjust offset and line string
+                $matchedString = $matches[0];
+                $offset += strlen($matchedString);
+                $line = substr($line, $offset);
+
+                $tokens[] = $token;
+
+                // Reset i to start from the beginning again of the token definitions.
+                $i = -1;
+            }
+
+            $i++;
+        }
+
+        // Check if the whole line was tokenized
+        if (strlen($line) > 0) {
+            throw new \InvalidArgumentException(
+                "Could not parse token starting at offset " . $offset . " on line " . $lineNumber . 
+                " No token definition found that matches: " . $line
+            );
+        }
+
+        return $tokens;
     }
 
     ////////////////////////////////////////////////////////////////////////
--- tests/SM/Lexer/LexerTest.php
+++ tests/SM/Lexer/LexerTest.php
@@ -26,6 +26,17 @@ class LexerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(array(), $this->lexer->getTokenDefinitions());
     }
 
+    public function testLexerAcceptsSingleDefinition()
+    {
+        $tokenDef = new Token("regex", "identifier");
+
+        try {
+            $lexer = new Lexer($tokenDef);
+        } catch (Exception $e) {
+            $this->fail("Should accept single token definition");
+        }
+    }
+
     public function testLexerDoesNotAcceptInvalidTokenDefinition()
     {
         $this->setExpectedException("InvalidArgumentException");
@@ -38,4 +49,139 @@ class LexerTest extends \PHPUnit_Framework_TestCase
 
         $lexer = new Lexer($tokenDef);
     }
+
+    ////////////////////////////////////////////////////////////////////////
+    // Tokenizing 
+    ////////////////////////////////////////////////////////////////////////
+
+    public function testTokenizesSingleWord()
+    {
+        $text = "hallo";
+        $identifier = "T_Hello";
+
+        $definition = $this->createTokenDefinition("hallo", $identifier);
+
+        $lexer = new Lexer($definition);
+        $tokens = $lexer->tokenize($text);
+
+        $this->assertCount(1, $tokens, "Only one token in string. Not possible to parse more.");
+
+        // Test the returned token
+        $token = $tokens[0];
+        $this->assertTrue(is_a($token, "SM\Lexer\Token"), "Should be a object of class Token.");
+        $this->assertEquals(1, $token->getLine());
+        $this->assertEquals(0, $token->getOffset());
+        $this->assertEquals($identifier, $token->getIdentifier());
+        $tokenMatch = $token->getMatch();
+        $this->assertEquals("hallo", $tokenMatch[0]);
+    }
+
+    public function testIgnoresLeadingWhitespaces()
+    {
+        $text = "   hallo";
+        $identifier = "T_HELLO";
+
+        $definition = $this->createTokenDefinition("hallo", $identifier);
+
+        $lexer = new Lexer($definition);
+        $tokens = $lexer->tokenize($text);
+
+        $this->assertCount(1, $tokens);
+        $token = $tokens[0];
+        $this->assertEquals(1, $token->getLine());
+        $this->assertEquals(3, $token->getOffset());
+        $tokenMatch = $token->getMatch();
+        $this->assertEquals("hallo", $tokenMatch[0]);
+    }
+
+    public function testReturnsEmptyArrayWhenEmptyStringIsTokenized()
+    {
+        $text = "";
+
+        $definition = $this->createTokenDefinition("hallo", "T_HELLO");
+
+        $lexer = new Lexer($definition);
+        $tokens = $lexer->tokenize($text);
+
+        $this->assertCount(0, $tokens, "Should return empty array");
+    }
+ 
+    public function testReturnsEmptyArrayWhenWhitespaceOnlyStringIsTokenized()
+    {
+        $text = "        ";
+
+        $definition = $this->createTokenDefinition("hallo", "T_HELLO");
+
+        $lexer = new Lexer($definition);
+        $tokens = $lexer->tokenize($text);
+
+        $this->assertCount(0, $tokens, "Should return empty array");
+    }   
+
+    public function testTokenize2TokensOfSameTypeInARow()
+    {
+        $text = "hallohallo";
+        $identifier = "T_HELLO";
+
+        $definition = $this->createTokenDefinition("hallo", $identifier);
+
+        $lexer = new Lexer($definition);
+        $tokens = $lexer->tokenize($text);
+
+        $this->assertCount(2, $tokens, "Should have got 2 tokens out of the text");
+        foreach ($tokens as $token){
+            $this->assertEquals($identifier, $token->getIdentifier(), "Should be of type T_HELLO");
+        }
+    }
+
+    public function testTokenize2TokensOfSameTypeInARowWithWhitespaces()
+    {
+        $text = "hallo  hallo";
+        $identifier = "T_HELLO";
+
+        $definition = $this->createTokenDefinition("hallo", $identifier);
+
+        $lexer = new Lexer($definition);
+        $tokens = $lexer->tokenize($text);
+
+        $this->assertCount(2, $tokens, "Should have got 2 tokens out of the text");
+        foreach ($tokens as $token){
+            $this->assertEquals($identifier, $token->getIdentifier(), "Should be of type T_HELLO");
+        }
+    }
+
+    public function testTokenizesWithCaptureGroups()
+    {
+        $text = "INT: 123";
+        $identifier = "T_INTEGER";
+
+        $definition = $this->createTokenDefinition("INT: (\d+)", $identifier);
+
+        $lexer = new Lexer($definition);
+        $tokens = $lexer->tokenize($text);
+
+        $this->assertCount(1, $tokens, "Should match a number in the text");
+
+        $token = $tokens[0];
+        $match = $token->getMatch();
+        $this->assertEquals($text, $match[0]);
+        $this->assertEquals(123, $match[1], "Capture group should have matched numbers");
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+    // Helper Methods
+    ////////////////////////////////////////////////////////////////////////
+    
+    /**
+     * Creates a token that can be used as a definition. 
+     *
+     * @param string $regex         The regex that is used for the token
+     * @param string $identifier    The identifier of the token.
+     */
+    protected function createTokenDefinition($regex, $identifier)
+    {
+        $tokenDef = new Token($regex, $identifier);
+        return $tokenDef;
+    }
+    
 }   
